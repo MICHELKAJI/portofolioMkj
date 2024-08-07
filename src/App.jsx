6f2a699728ad884c3b317f0d93d6585f05be529e
Michel <fistonkajibwami2@gmail.com>
2024-08-07 18:54:45 -0330

    fixed scrolingtop

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Layout from "./layoute"
 import About from "./pages/about"
 import Projects from "./pages/portofolio"
 import PageContacts from "./pages/Pagecontacts"
+import ScrollToTop from "./components/scrolTop"
 
 
 function App (){
   return(
     <div>
       <Router>
+        <ScrollToTop/>
       <Layout/>
       <Routes>
         <Route path="/" element={<Home/>}/>
